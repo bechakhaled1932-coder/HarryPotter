7b12024fa8acbac520b95434b001a061f6654c02
bechakhaled1932-coder <bechakhaled1932@gmail.com>
2026-06-23 17:30:31 +0100

    initialisation du navbar

---
+++ harry-potter-site/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>⚡ Hogwarts</h1>
+      <ul>
+        <li><a href="#houses">Houses</a></li>
+        <li><a href="#characters">Characters</a></li>
+        <li><a href="#quiz">Quiz</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
